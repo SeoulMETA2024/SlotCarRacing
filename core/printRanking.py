8b1f8efb024c7969002878b023b0acd4144e4ff7
NoelMeta <balinesenoel@gmail.com>
2024-12-25 17:50:18 +0900

    주석추가

--- core/printRanking.py
+++ core/printRanking.py
@@ -2,6 +2,6 @@ import pyfiglet
 
 def print_data(place, nickname,time) -> None:
 
-    art = pyfiglet.figlet_format(f"{place} : {nickname}:{time}\n")
+    art = pyfiglet.figlet_format(f"{place} : {nickname}:{time}\n")#줄띄우기가 먹는지 안먹는지 모름
     print(art + '\n\n\n\n\n\n\n\n\n')
     return None
