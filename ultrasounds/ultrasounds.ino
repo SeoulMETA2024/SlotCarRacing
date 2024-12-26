1add83c9bc904f6a64be444adcdc40e65f663f3e
NoelMeta <balinesenoel@gmail.com>
2024-12-26 09:43:37 +0900

    d

--- ultrasounds/ultrasounds.ino
+++ ultrasounds/ultrasounds.ino
@@ -2,6 +2,10 @@ int echo = 8;
 int trig = 11;
 unsigned long preTime = 0;
 unsigned long nowTime = 0;
+float cycletime;
+float nowdistance;
+float predistance;
+float carandwalldistance;
 
 void setup() {
   Serial.begin(9600);
@@ -10,34 +14,35 @@ void setup() {
 }
 
 void loop() {
-  float cycletime;
-  float distance;
+  
   
   digitalWrite(trig, HIGH);
   delay(10);
   digitalWrite(trig, LOW);
   
-  cycletime = pulseIn(echo, HIGH); 
+  cycletime = pulseIn(echo, HIGH);
+  predistance = nowdistance; 
   
-  distance = ((340 * cycletime) / 10000) / 2;  
+  nowdistance = ((340 * cycletime) / 10000) / 2;  
 
   //Serial.print("Distance:");
-  Serial.println(distance);
+  //Serial.println(distance);
   //Serial.print("cm"); 
   delay(500);
 
 
-  
-  if (/*센서작동할때*/) //이거 만들기!
-  {
-        preTime = nowTime;
-        unsigned long nowTime = millis();
-        int Time = nowTime - preTime ;
-        char a[20];
-        itoa(Time,a,10);
-        //char b[] = "시간 측정 시작";
-        //printf("%s\n",b);
-      }
+  if(predistance <carandwalldistance && nowdistance >carandwalldistance ){
+    
+          preTime = nowTime;
+          unsigned long nowTime = millis();
+          int Time = nowTime - preTime ;
+          char a[20];
+          itoa(Time,a,10);
+          //char b[] = "시간 측정 시작";
+          //printf("%s\n",b);
+      
+
+  }
 
       
     }
