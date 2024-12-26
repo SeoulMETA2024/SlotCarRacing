9f4e005e2a6ed2c194510c571d63f20f01d2e96c
NoelMeta <balinesenoel@gmail.com>
2024-12-26 12:45:39 +0900

    계수 다시 조정

--- motor/motor.ino
+++ motor/motor.ino
@@ -6,7 +6,7 @@
 #define speedpin 6
 #define button 9
 
-const int THRESHOLD = 500; // 임계값 0.5초
+const int THRESHOLD = 5000; // 임계값 0.5초
 int speed = 200;
 float oldTime = 0.0f;
 float currentTime = 0.0f;
@@ -36,8 +36,8 @@ void loop() {
         // 로그 함수를 이용한 속도 제어
         float x = THRESHOLD - deltaTime;
         if(x > 0) {
-            int y = (int)log(x*100) ;//계수 조정
-            speed = constrain(y*255, 0, 255); // 속도값 범위 제한,계수조정
+            int y = (int)70*log(x+1) ;//계수 조정
+            speed = constrain(y, 0, 255); // 속도값 범위 제한,계수조정
             
 
 
